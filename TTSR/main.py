7f56cdc0b52b7e9ab555c0c235c08998fbfef0a6
nahye03 <nahelove03@gmail.com>
2023-04-10 09:29:10 +0900

    Initial commit

---
+++ TTSR/main.py
@@ -0,0 +1,46 @@
+from option import args
+from utils import mkExpDir
+from dataset import dataloader
+from model import TTSR
+from loss.loss import get_loss_dict
+from trainer import Trainer
+
+import os
+import torch
+import warnings
+warnings.filterwarnings('ignore')
+
+
+if __name__ == '__main__':
+    ### make save_dir
+    _logger = mkExpDir(args)
+
+    ### dataloader of training set and testing set
+    _dataloader = dataloader.get_dataloader(args) if (not args.test) else None
+
+    ### device and model
+    device = torch.device('cpu' if args.cpu else 'cuda')
+    _model = TTSR.TTSR(args).to(device)
+    if ((not args.cpu) and (args.num_gpu > 1)):
+        _model = nn.DataParallel(_model, list(range(args.num_gpu)))
+
+    ### loss
+    _loss_all = get_loss_dict(args, _logger)
+
+    ### trainer
+    t = Trainer(args, _logger, _dataloader, _model, _loss_all)
+
+    ### test / eval / train
+    if (args.test):
+        t.load(model_path=args.model_path)
+        t.test()
+    elif (args.eval):
+        t.load(model_path=args.model_path)
+        t.evaluate()
+    else:
+        for epoch in range(1, args.num_init_epochs+1):
+            t.train(current_epoch=epoch, is_init=True)
+        for epoch in range(1, args.num_epochs+1):
+            t.train(current_epoch=epoch, is_init=False)
+            if (epoch % args.val_every == 0):
+                t.evaluate(current_epoch=epoch)
